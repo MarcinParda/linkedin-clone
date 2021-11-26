7860a222e31eafb576345913f23cd227264487fc
MarcinParda <marcin98parda@gmail.com>
2021-11-26 20:46:52 +0100

    add styled components for navbar

---
+++ src/components/Login.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+const Login = (props) => {
+    return(
+        <Container>
+            <Nav>
+                <a href="/">
+                    <img src="/images/login-logo.svg" alt="login logo"/>
+                </a>
+                <div>
+                    <Join>Join now</Join>
+                    <SignIn>Sign in</SignIn>
+                </div>
+            </Nav>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+  padding: 0;
+`;
+
+const Nav = styled.div`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+
+const Join = styled.a`
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
+`;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition: 167ms;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: #fff;
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+
+export default Login;
