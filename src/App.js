c1a6dad4e5083ff162d2f8f9aa2bba2556dc83f5
MarcinParda <marcin98parda@gmail.com>
2021-11-27 21:21:06 +0100

    add searchbar styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Login from "components/Login";
+import Header from "components/Header";
+import Home from "components/Home";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
             <Switch>
                 <Route exact path="/" >
                     <Login />
+                </Route>
+                <Route path="/home">
+                    <Header />
+                    <Home />
                 </Route>
             </Switch>
         </Router>
