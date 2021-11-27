0c64073c3d7f0557f9b0e11b99412f05329e509e
MarcinParda <marcin98parda@gmail.com>
2021-11-28 00:13:33 +0100

    add redux and main page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,9 +16,9 @@ const Home = (props) => {
                 </p>
             </Section>
             <Layout>
-                <Leftside />
-                {/*<Main />*/}
-                <RightSide />
+                <Leftside/>
+                <Main/>
+                <RightSide/>
             </Layout>
         </Container>
     );
@@ -43,18 +43,22 @@ const Section = styled.section`
   text-decoration: underline;
   display: flex;
   justify-content: center;
+
   h5 {
     color: #0a66c2;
     font-size: 14px;
+
     a {
       font-weight: 700;
     }
   }
+
   p {
     font-size: 14px;
     color: #434649;
     font-weight: 600;
   }
+
   @media (max-width: 768px) {
     flex-direction: column;
     padding: 0 5px;
